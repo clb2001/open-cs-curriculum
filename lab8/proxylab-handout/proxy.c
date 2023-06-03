3720cfb5399b6bc48be8556ae9ad476c2a90be47
Libin Cheng <clb245000@163.com>
2023-06-03 14:17:42 +0800

    2023.6.3

--- lab8/proxylab-handout/proxy.c
+++ lab8/proxylab-handout/proxy.c
@@ -20,8 +20,6 @@
 #define NTHREADS 4
 #define SBUFSIZE 16
 
-sbuf_t sbuf;
-
 typedef struct{
     char method[MAX_METHOD_LEN];
     char host[MAX_HOST_LEN];
@@ -68,9 +66,17 @@ void read_request_line(rio_t *rio, char *buf, RequestLine *request_line);
 void parse_uri(char *uri, RequestLine *request_line);
 void read_request_header(rio_t *rio, char *buf, RequestHeader *request_header);
 int send_request(Request *request);
-void forward_response(int fd, int clientfd);
+void forward_response(int fd, int clientfd, Request *request);
 void sigpipe_handler(int sig);
 void *thread(void *vargp);
+void cache_init(Cache *cache, int n);
+int cache_find(Cache *cache, Request *request, int fd);
+void cache_insert(Cache *cache, Request *request, char *response);
+
+int cur_time_stamp = 0;
+sbuf_t sbuf;
+// mustn't use *cache, or define it in main()
+Cache cache;
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
 int main(int argc, char **argv){
@@ -87,6 +93,9 @@ int main(int argc, char **argv){
     }
     Signal(SIGPIPE, sigpipe_handler);
 
+    // define cache as a global variable
+    // Cache *cache = (Cache*)malloc(sizeof(Cache));
+    cache_init(&cache, MAX_CACHE_SIZE/MAX_OBJECT_SIZE);
     sbuf_init(&sbuf, SBUFSIZE);
     for (int i = 0; i < NTHREADS; i++){
         Pthread_create(&tid, NULL, thread, NULL);
@@ -100,8 +109,6 @@ int main(int argc, char **argv){
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE,
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        // doit(connfd);
-        // Close(connfd);
     }
     exit(0);
 }
@@ -121,9 +128,11 @@ void *thread(void *vargp){
 void doit(int fd){
     Request request;
     read_request(fd, &request);
-    int clientfd = send_request(&request);
-    forward_response(fd, clientfd);
-    Close(clientfd);
+    if (!cache_find(&cache, &request, fd)){
+        int clientfd = send_request(&request);
+        forward_response(fd, clientfd, &request);
+        Close(clientfd);
+    }
 }
 
 void read_request(int fd, Request *request){
@@ -167,7 +176,7 @@ void parse_uri(char *uri, RequestLine *request_line){
         fprintf(stderr, "Error: invalid uri!\n");
     }
     uri += strlen("http://");// maybe: localhost:15213/home.html
-    // IF port is NULL, default 80, like this: localhost/home.html
+    // if port is NULL, default 80, like this: localhost/home.html
     if ((port_start = strstr(uri, ":")) == NULL){
         strcpy(request_line->port, "80");
     }
@@ -225,18 +234,23 @@ int send_request(Request *request){
     // first line, eg: GET /hub/index.html HTTP/1.0
     sprintf(content, "%s %s HTTP/1.0\r\n", request_line->method, request_line->path);
     Rio_writen(clientfd, content, strlen(content));
+
     // second line, eg: Host: www.cmu.edu
     sprintf(content, "Host: %s%s\r\n", request_line->host, request_line->port);
     Rio_writen(clientfd, content, strlen(content));    
+    
     // third line, eg: User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36
     sprintf(content, "User-Agent: %s\r\n", user_agent_hdr);
     Rio_writen(clientfd, content, strlen(content));    
+    
     // fourth line: Connection: close
     sprintf(content, "Connection: close\r\n");
     Rio_writen(clientfd, content, strlen(content));   
+    
     // fifth line: Proxy-Connection: close  
     sprintf(content, "Proxy-Connection: close\r\n");
     Rio_writen(clientfd, content, strlen(content));
+    
     // other infomation that need not to change
     for (int i = 0; i < request->request_header_num; i++){
         char *name = request->request_header[i].name;
@@ -256,14 +270,87 @@ int send_request(Request *request){
     return clientfd;
 }
 
-void forward_response(int fd, int clientfd){
+void forward_response(int fd, int clientfd, Request *request){
     size_t n;
-    char buf[MAXLINE];
+    char buf[MAXLINE], content[MAX_OBJECT_SIZE];
     rio_t rio;
+    int response_bytes = 0;
     Rio_readinitb(&rio, clientfd);
     while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0){
         Rio_writen(fd, buf, n);
+        if (response_bytes + n <= MAX_OBJECT_SIZE){
+            strcpy(content + response_bytes, buf);
+        }
+        response_bytes += n;
     }
+    if (response_bytes <= MAX_OBJECT_SIZE){
+        cache_insert(&cache, request, content);
+    }
+}
+
+void cache_init(Cache *cache, int n){
+    Sem_init(&cache->mutex, 0, 1);
+    cache->cache_num = n;
+    cache->cache_set = Calloc(n, sizeof(CachePair));
+}
+
+int cache_find(Cache *cache, Request *request, int fd){
+    RequestLine *request_line = &request->request_line;
+    char match_str[MAXLINE];
+    int cache_hit = 0;
+    CachePair *cache_pair;
+    sprintf(match_str, "%s http://%s:%s%s HTTP/1.0", request_line->method, request_line->host, request_line->port, request_line->path);
+    P(&cache->mutex);
+    for (int i = 0; i < cache->cache_num; i++){
+        cache_pair = cache->cache_set + i;// pay attention to this usage
+        if (!cache_pair->valid){
+            continue;
+        }
+        if (!strcmp(match_str, cache_pair->request)){
+            Rio_writen(fd, cache_pair->response, MAX_OBJECT_SIZE);
+            cache_pair->time_stamp = cur_time_stamp++;
+            cache_hit = 1;
+            break;
+        }
+    }
+    V(&cache->mutex);
+    if (cache_hit){
+        printf("Cache hit: http://%s:%s%s\n\n", request_line->host, request_line->port, request_line->path);
+    }
+    return cache_hit;
+}
+
+void cache_insert(Cache *cache, Request *request, char *response){
+    RequestLine *request_line = &request->request_line;
+    char match_str[MAXLINE];
+    CachePair *cache_pair, *old_pair;
+    int vacancy = 0;
+    int oldest_time_stamp = 0x7fffffff;
+    sprintf(match_str, "%s http://%s:%s%s HTTP/1.0", request_line->method, request_line->host, request_line->port, request_line->path);
+    P(&cache->mutex);
+    for (int i = 0; i < cache->cache_num; i++){
+        cache_pair = cache->cache_set + i;
+        if (!cache_pair->valid){
+            strcpy(cache_pair->request, match_str);
+            strcpy(cache_pair->response, response);
+            cache_pair->time_stamp = cur_time_stamp++;
+            cache_pair->valid = 1;
+            vacancy = 1;
+            break;
+        }
+        // get the smallest time_stamp in cache_set
+        if (oldest_time_stamp > cache_pair->time_stamp){
+            oldest_time_stamp = cache_pair->time_stamp;
+            old_pair = cache_pair;
+        }
+    }
+    if (!vacancy){
+        old_pair->time_stamp = 0;
+        old_pair->valid = 1;
+        strcpy(old_pair->response, response);
+        strcpy(old_pair->request, match_str); 
+    }
+    V(&cache->mutex);
 }
 
 void sigpipe_handler(int sig){
