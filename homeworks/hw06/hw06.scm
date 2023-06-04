8e5e58073ce3bf38c9c6ffd66124d8e9f470d48f
Libin Cheng <clb245000@163.com>
2023-06-04 23:46:07 +0800

    2023.6.4

--- homeworks/hw06/hw06.scm
+++ homeworks/hw06/hw06.scm
@@ -16,10 +16,10 @@
 (define (square x) (* x x))
 
 ; how to use square?
-(define (pow x y)
-  (cond((= 1 x) 1)((= 0 y) 1)((= 1 y) x)((= 0 (modulo y 2))(* (pow x (quotient y 2)) (pow x (quotient y 2))))(else (* x (* (pow x (quotient (- y 1) 2)) (pow x (quotient (- y 1) 2))))))
-)
-
 ;(define (pow x y)
-;  (cond((= 0 y) 1)((= 1 y) x)(else (* x (* (pow x (quotient (- y 1) 2)) (pow x (quotient (- y 1) 2))))))
+;  (cond((= 1 x) 1)((= 0 y) 1)((= 1 y) x)((= 0 (modulo y 2))(* (pow x (quotient y 2)) (pow x (quotient y 2))))(else (* x (* (pow x (quotient (- y 1) 2)) (pow x (quotient (- y 1) 2))))))
 ;)
+
+(define (pow x y)
+  (cond((= 1 x) 1)((= 0 y) 1)((= 1 y) x)((= 0 (modulo y 2))(square (pow x (quotient y 2))))(else (* x (square(pow x (quotient (- y 1) 2))))))
+)
