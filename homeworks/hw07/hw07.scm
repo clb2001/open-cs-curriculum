1645cd10254a5b016e0815956961503d946fa4a0
Libin Cheng <clb245000@163.com>
2023-06-06 02:40:13 +0800

    2023.6.6

--- homeworks/hw07/hw07.scm
+++ homeworks/hw07/hw07.scm
@@ -1,5 +1,9 @@
 (define (filter-lst fn lst)
-  'YOUR-CODE-HERE
+  (cond ((null? lst) '())
+        ((fn (car lst))
+         (cons (car lst)
+               (filter-lst fn (cdr lst))))
+        (else (filter-lst fn (cdr lst))))
 )
 
 ;;; Tests
@@ -8,9 +12,23 @@
 (filter-lst even? '(0 1 1 2 3 5 8))
 ; expect (0 2 8)
 
+;cons 过程接受两个参数，第一个参数是要插入到列表中的元素，第二个参数是列表本身。它返回一个新的列表，该列表以第一个参数作为头部，第二个参数作为尾部。
+;在Scheme中，member是一个内置过程，用于检查一个元素是否在列表中，并返回第一次出现该元素的子列表。
+;(define (interleave first second)
+;  (cond ((null? first) second)
+;        ((member (car first) second)
+;         (interleave (cdr first) second))
+;        (else
+;         (cons (car first) 
+;               (interleave (cdr first) second))))
+;)
 
 (define (interleave first second)
-  'YOUR-CODE-HERE
+  (cond ((null? first) second)
+       ((null? second) first)
+       (else (cons (car first)
+                   (cons (car second)
+                         (interleave (cdr first) (cdr second))))))
 )
 
 (interleave (list 1 3 5) (list 2 4 6))
@@ -24,11 +42,23 @@
 
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (cond ((= 0 (term n)) start)
+        (else (combiner (term n) (accumulate combiner start (- n 1) term))))
 )
 
 
+(define (element-in-list? element lst)
+  (cond ((null? lst) #f)
+       ((eq? element (car lst)) #t)
+       (else (element-in-list? element (cdr lst))))
+)
+
 (define (no-repeats lst)
-  'YOUR-CODE-HERE
+  (define (helper input result)
+    (cond ((null? input) result)
+          ((element-in-list? (car input) result)
+           (helper (cdr input) result))
+          (else (helper (cdr input) (append result (list (car input)))))))
+  (helper lst '())
 )
 
