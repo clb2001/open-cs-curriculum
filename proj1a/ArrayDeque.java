41c68a085ff14f740a056211c6c684669b84aa1d
clb2001 <clb245000@163.com>
2023-07-04 23:55:46 +0800

    20230704

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,21 @@
+public class ArrayDeque {
+    public ArrayDeque(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public int get(){
+
+    }
+
+    public int size(){
+
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,29 @@
+public class LinkedListDeque {
+    public LinkedListDeque(){
+
+    }
+
+    public <T> T getRecursive(int index){
+
+    }
+
+    public void ArrayDeque(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public int get(){
+
+    }
+
+    public int size(){
+
+    }
+}
