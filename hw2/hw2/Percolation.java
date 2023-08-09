eb90ae691607ceda51df2bb0e9980e59c7c506d4
clb2001 <clb245000@163.com>
2023-08-10 00:31:32 +0800

    20230810

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -7,23 +7,39 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  */
 public class Percolation {
     // create N-by-N grid, with all sites initially blocked
-    public Percolation(int N) {
+    int[][] grid;
+    int size;
 
+    public Percolation(int N) {
+        try {
+            if (N <= 0) {
+                throw new IllegalArgumentException();
+            }
+            grid = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    grid[i][j] = 0;
+                }
+            }
+            size = N;
+        } catch (IllegalArgumentException e){
+            System.out.println("Exception!");
+        }
     }
 
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-
+        grid[row][col] = 1;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return true;
+        return grid[row][col] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return true;
+        return grid[row][col] == 0;
     }
 
     // number of open sites
@@ -33,7 +49,12 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        for (int i = 0; i < size; i++) {
+            if (grid[0][i] == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // use for unit testing (not required)
---
+++ hw2/hw2/WeightedQuickUnionUF.java
@@ -0,0 +1,7 @@
+package hw2;
+
+/**
+ * @author chenglibin
+ */
+public class WeightedQuickUnionUF {
+}
