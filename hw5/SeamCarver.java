569b2ebfa4b795d917b0b1ce4e00a73e48a9b987
clb2001 <clb245000@163.com>
2023-08-21 23:19:16 +0800

    20220821

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -6,13 +6,11 @@ public class SeamCarver {
     private Picture picture;
     private int width;
     private int height;
-    private boolean transpose;
 
     public SeamCarver(Picture picture) {
         this.picture = picture;
         this.width = picture.width();
         this.height = picture.height();
-        this.transpose = false;
     }
 
     // current picture
@@ -40,17 +38,10 @@ public class SeamCarver {
         int[] rgbUp;
         int[] rgbDown;
 
-        if (!transpose) {
-            rgbRight = (x < width - 1 ? getRGBArr(x + 1, y) : getRGBArr(0, y));
-            rgbLeft = (x > 0 ? getRGBArr(x - 1, y) : getRGBArr(width - 1, y));
-            rgbUp = (y < height - 1 ? getRGBArr(x, y + 1) : getRGBArr(x, 0));
-            rgbDown = (y > 0 ? getRGBArr(x, y - 1) : getRGBArr(x, height - 1));
-        } else {
-            rgbUp = (x < height - 1 ? getRGBArr(x + 1, y) : getRGBArr(0, y));
-            rgbDown = (x > 0 ? getRGBArr(x - 1, y) : getRGBArr(height - 1, y));
-            rgbRight = (y < width - 1 ? getRGBArr(x, y + 1) : getRGBArr(x, 0));
-            rgbLeft = (y > 0 ? getRGBArr(x, y - 1) : getRGBArr(x, width - 1));
-        }
+        rgbRight = (x < width - 1 ? getRGBArr(x + 1, y) : getRGBArr(0, y));
+        rgbLeft = (x > 0 ? getRGBArr(x - 1, y) : getRGBArr(width - 1, y));
+        rgbUp = (y < height - 1 ? getRGBArr(x, y + 1) : getRGBArr(x, 0));
+        rgbDown = (y > 0 ? getRGBArr(x, y - 1) : getRGBArr(x, height - 1));
 
         double Rx = Math.pow(rgbRight[0] - rgbLeft[0], 2);
         double Gx = Math.pow(rgbRight[1] - rgbLeft[1], 2);
@@ -63,57 +54,105 @@ public class SeamCarver {
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        double[][] e = new double[width][height];
+        double[][] e = new double[height][width];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                e[i][j] = energy(i, j);
+                e[j][i] = energy(i, j);
             }
         }
 
-        double[][] minEnergy = new double[width][height];
-        int[][] minPath = new int[width][height];
-        for (int i = 0; i < width; i++) {
+        double[][] minEnergy = new double[height][width];
+        int[][] minPath = new int[height][width];
+        for (int i = 0; i < height; i++) {
             minEnergy[i][0] = e[i][0];
-            minPath[i][0] = i;
+            minPath[i][0] = -1;
         }
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 1; j < height; j++) {
+        // 这里应该用自己熟悉的寻址方式写
+        // 这里的循环顺序很有讲究
+        for (int j = 1; j < width; j++) {
+            for (int i = 0; i < height; i++) {
                 if (i == 0) {
                     minEnergy[i][j] = e[i][j] + Math.min(minEnergy[i][j - 1], minEnergy[i + 1][j - 1]);
-                    minPath[i][j] = (minEnergy[i][j - 1] < minEnergy[i + 1][j - 1])? i : i + 1;
-                } else if (i == width - 1) {
-                    minEnergy[i][j] = e[i][j] + Math.min(minEnergy[i - 1][j - 1], minEnergy[i][j - 1]);
-                    minPath[i][j] = (minEnergy[i][j - 1] < minEnergy[i - 1][j - 1])? i : i - 1;
+                    minPath[i][j] = (minEnergy[i][j - 1] < minEnergy[i + 1][j - 1]? i : i + 1);
+                } else if (i == height - 1) {
+                    minEnergy[i][j] = e[i][j] + Math.min(minEnergy[i][j - 1], minEnergy[i - 1][j - 1]);
+                    minPath[i][j] = (minEnergy[i][j - 1] < minEnergy[i - 1][j - 1]? i : i - 1);
                 } else {
                     double tmp = Math.min(minEnergy[i - 1][j - 1], minEnergy[i + 1][j - 1]);
-                    minPath[i][j] = (minEnergy[i + 1][j - 1] < minEnergy[i - 1][j - 1])? i + 1 : i - 1;
-                    minEnergy[i][j] = e[i][j] + Math.min(tmp, minEnergy[i - 1][j - 1]);
-                    minPath[i][j] = (minEnergy[i][j] < minPath[i][j])? i : minPath[i][j];
+                    minPath[i][j] = (minEnergy[i - 1][j - 1] < minEnergy[i + 1][j - 1]? i - 1 : i + 1);
+                    minEnergy[i][j] = e[i][j] + Math.min(tmp, minEnergy[i][j - 1]);
+                    minPath[i][j] = (tmp < minEnergy[i][j - 1]? minPath[i][j] : i);
                 }
             }
         }
 
         int flag = 0;
-        double cost = minEnergy[0][height - 1];
-        for (int i = 1; i < width; i++) {
-            if (minEnergy[i][height - 1] < cost) {
+        double cost = minEnergy[0][width - 1];
+        for (int i = 1; i < height; i++) {
+            if (minEnergy[i][width - 1] < cost) {
                 flag = i;
-                cost = minEnergy[i][height - 1];
+                cost = minEnergy[i][width - 1];
             }
         }
-
-        return minPath[flag];
+        int[] res = new int[width];
+        res[width - 1] = flag;
+        for (int i = width - 2; i != -1; i--) {
+            flag = minPath[flag][i + 1];
+            res[i] = flag;
+        }
+        return res;
     }
 
     // sequence of indices for vertical seam
     // refer to other answers to avoid copying.
     public int[] findVerticalSeam() {
-        transpose = true;
-        swap();
-        int[] res = findHorizontalSeam();
-        swap();
-        transpose = false;
+        double[][] e = new double[height][width];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                e[j][i] = energy(i, j);
+            }
+        }
+
+        double[][] minEnergy = new double[height][width];
+        int[][] minPath = new int[height][width];
+        for (int i = 0; i < width; i++) {
+            minEnergy[0][i] = e[0][i];
+            minPath[0][i] = -1;
+        }
+
+        // 这里应该用自己熟悉的寻址方式写
+        for (int i = 1; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j == 0) {
+                    minEnergy[i][j] = e[i][j] + Math.min(minEnergy[i - 1][j], minEnergy[i - 1][j + 1]);
+                    minPath[i][j] = (minEnergy[i - 1][j] < minEnergy[i - 1][j + 1]? j : j + 1);
+                } else if (j == width - 1) {
+                    minEnergy[i][j] = e[i][j] + Math.min(minEnergy[i - 1][j - 1], minEnergy[i - 1][j]);
+                    minPath[i][j] = (minEnergy[i - 1][j - 1] < minEnergy[i - 1][j]? j - 1 : j);
+                } else {
+                    double tmp = Math.min(minEnergy[i - 1][j - 1], minEnergy[i - 1][j + 1]);
+                    minPath[i][j] = (minEnergy[i - 1][j + 1] < minEnergy[i - 1][j - 1]? j + 1 : j - 1);
+                    minEnergy[i][j] = e[i][j] + Math.min(tmp, minEnergy[i - 1][j]);
+                    minPath[i][j] = (tmp < minEnergy[i - 1][j]? minPath[i][j] : j);
+                }
+            }
+        }
+
+        int flag = 0;
+        double cost = minEnergy[height - 1][0];
+        for (int i = 1; i < width; i++) {
+            if (minEnergy[height - 1][i] < cost) {
+                flag = i;
+                cost = minEnergy[height - 1][i];
+            }
+        }
+        int[] res = new int[height];
+        res[height - 1] = flag;
+        for (int i = height - 2; i != -1; i--) {
+            flag = minPath[i + 1][flag];
+            res[i] = flag;
+        }
         return res;
     }
 
@@ -153,11 +192,5 @@ public class SeamCarver {
         arr[1] = (rgb >> 8) & 0xFF;
         arr[2] = (rgb) & 0xFF;
         return arr;
-    }
-
-    private void swap() {
-        int tmp = height;
-        height = width;
-        width = tmp;
     }
 }
